0bab8d25d3d6e2dad23133f9b02be07e3827e6e6
sumanthculli <sumanth.culli@gmail.com>
2024-08-03 11:06:56 -0500

    Create main.tf

---
+++ secretsmanager/tfscripts/lambda/main.tf
@@ -0,0 +1,95 @@
+# Lambda function for RDS PostgreSQL password rotation
+resource "aws_lambda_function" "rds_postgres_rotation" {
+  filename         = "lambda_function.zip"
+  function_name    = "rds-postgres-rotation-function"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  timeout          = 30
+  memory_size      = 128
+
+  vpc_config {
+    subnet_ids         = var.private_subnet_ids
+    security_group_ids = [var.lambda_security_group_id]
+  }
+
+  environment {
+    variables = {
+      SECRETS_MANAGER_ENDPOINT = "https://secretsmanager.${var.aws_region}.amazonaws.com"
+    }
+  }
+}
+
+# IAM role for the Lambda function
+resource "aws_iam_role" "lambda_role" {
+  name = "rds-postgres-rotation-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# IAM policy for the Lambda function
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "rds-postgres-rotation-lambda-policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:PutSecretValue",
+          "secretsmanager:UpdateSecretVersionStage"
+        ]
+        Resource = var.secret_arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+# Attach the AWSLambdaVPCAccessExecutionRole managed policy
+resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+# Configure secret rotation
+resource "aws_secretsmanager_secret_rotation" "rotation" {
+  secret_id           = var.secret_arn
+  rotation_lambda_arn = aws_lambda_function.rds_postgres_rotation.arn
+
+  rotation_rules {
+    automatically_after_days = 30
+  }
+}
