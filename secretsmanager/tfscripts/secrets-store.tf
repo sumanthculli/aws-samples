c407175bc8f8d1d850060322e7c2ddf52451c826
sumanthculli <sumanth.culli@gmail.com>
2024-08-04 23:48:53 -0500

    Create secrets-store.tf

---
+++ secretsmanager/tfscripts/secrets-store.tf
@@ -0,0 +1,6 @@
+resource "helm_release" "csi_secrets_store" {
+  name       = "csi-secrets-store"
+  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+  chart      = "secrets-store-csi-driver"
+  namespace  = "kube-system"
+}
