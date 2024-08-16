d94919f8001d3af706a220948b30c83b9698e70f
sumanthculli <sumanth.culli@gmail.com>
2024-08-16 08:21:53 -0500

    Create argoHelm.tf

---
+++ tfcode/argoHelm.tf
@@ -0,0 +1,36 @@
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "5.46.7"
+
+  values = [file("${path.module}/values.yaml")]
+
+  set {
+    name  = "server.service.type"
+    value = "ClusterIP"
+  }
+
+  set {
+    name  = "server.ingress.enabled"
+    value = "false"
+  }
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = "your-eks-cluster-name"
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = "your-eks-cluster-name"
+}
