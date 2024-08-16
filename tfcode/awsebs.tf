3db86cc41d5aab4612d1a167edbb6127b15151dd
sumanthculli <sumanth.culli@gmail.com>
2024-08-16 08:12:09 -0500

    Create awsebs.tf

---
+++ tfcode/awsebs.tf
@@ -0,0 +1,103 @@
+# EBS CSI Driver
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name = aws_eks_cluster.main.name
+  addon_name   = "aws-ebs-csi-driver"
+  addon_version = "LATEST"
+  service_account_role_arn = aws_iam_role.ebs_csi_role.arn
+}
+
+resource "aws_iam_role" "ebs_csi_role" {
+  name = "eks-ebs-csi-driver-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${aws_iam_openid_connect_provider.eks.url}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi_role.name
+}
+
+# EFS CSI Driver
+resource "aws_eks_addon" "efs_csi" {
+  cluster_name = aws_eks_cluster.main.name
+  addon_name   = "aws-efs-csi-driver"
+  addon_version = "LATEST"
+  service_account_role_arn = aws_iam_role.efs_csi_role.arn
+}
+
+resource "aws_iam_role" "efs_csi_role" {
+  name = "eks-efs-csi-driver-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${aws_iam_openid_connect_provider.eks.url}:sub": "system:serviceaccount:kube-system:efs-csi-controller-sa"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "efs_csi_policy" {
+  name = "eks-efs-csi-driver-policy"
+  role = aws_iam_role.efs_csi_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticfilesystem:DescribeAccessPoints",
+          "elasticfilesystem:DescribeFileSystems",
+          "elasticfilesystem:DescribeMountTargets",
+          "ec2:DescribeAvailabilityZones"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticfilesystem:CreateAccessPoint"
+        ]
+        Resource = "*"
+        Condition = {
+          StringLike = {
+            "aws:RequestTag/efs.csi.aws.com/cluster": "true"
+          }
+        }
+      },
+      {
+        Effect = "Allow"
+        Action = "elasticfilesystem:DeleteAccessPoint"
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "aws:ResourceTag/efs.csi.aws.com/cluster": "true"
+          }
+        }
+      }
+    ]
+  })
+}
