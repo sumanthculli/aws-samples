908699ced92d6bbaadd8a347fadfb54c5f79bbec
sumanthculli <sumanth.culli@gmail.com>
2026-02-10 12:31:44 -0600

    Update nlb_alarms.tf

--- nlb_alarms.tf
+++ nlb_alarms.tf
@@ -72,3 +72,117 @@ resource "aws_cloudwatch_metric_alarm" "nlb_alarms" {
   
   alarm_actions = [aws_lambda_function.nlb_handler.arn]
 }
+
+
+
+
+# Anomaly detector for ProcessedBytes
+resource "aws_cloudwatch_metric_alarm" "nlb_processed_bytes_anomaly" {
+  alarm_name          = "nlb-processed-bytes-anomaly"
+  comparison_operator = "LessThanLowerOrGreaterThanUpperThreshold"
+  evaluation_periods  = 2
+  threshold_metric_id = "ad1"
+  alarm_description   = "Anomaly detection for NLB ProcessedBytes"
+  treat_missing_data  = "notBreaching"
+  
+  metric_query {
+    id          = "m1"
+    return_data = true
+    
+    metric {
+      metric_name = "ProcessedBytes"
+      namespace   = "AWS/NetworkELB"
+      period      = 300
+      stat        = "Sum"
+    }
+  }
+  
+  metric_query {
+    id          = "ad1"
+    expression  = "ANOMALY_DETECTION_BAND(m1, 2)"
+    label       = "ProcessedBytes (expected)"
+    return_data = true
+  }
+  
+  alarm_actions = [aws_lambda_function.nlb_handler.arn]
+}
+
+# Anomaly detector for TCP_Target_Reset_Count
+resource "aws_cloudwatch_metric_alarm" "nlb_target_reset_anomaly" {
+  alarm_name          = "nlb-target-reset-anomaly"
+  comparison_operator = "GreaterThanUpperThreshold"
+  evaluation_periods  = 2
+  threshold_metric_id = "ad1"
+  alarm_description   = "Anomaly detection for NLB TCP Target Reset Count"
+  treat_missing_data  = "notBreaching"
+  
+  metric_query {
+    id          = "m1"
+    return_data = true
+    
+    metric {
+      metric_name = "TCP_Target_Reset_Count"
+      namespace   = "AWS/NetworkELB"
+      period      = 300
+      stat        = "Sum"
+    }
+  }
+  
+  metric_query {
+    id          = "ad1"
+    expression  = "ANOMALY_DETECTION_BAND(m1, 2)"
+    label       = "TCP_Target_Reset_Count (expected)"
+    return_data = true
+  }
+  
+  alarm_actions = [aws_lambda_function.nlb_handler.arn]
+}
+
+# Using Metric Insights with Anomaly Detection
+resource "aws_cloudwatch_metric_alarm" "nlb_processed_bytes_insight_anomaly" {
+  alarm_name          = "nlb-processed-bytes-insight-anomaly"
+  comparison_operator = "LessThanLowerOrGreaterThanUpperThreshold"
+  evaluation_periods  = 2
+  threshold_metric_id = "ad1"
+  alarm_description   = "Anomaly detection for ProcessedBytes across all NLBs using metric insights"
+  treat_missing_data  = "notBreaching"
+  
+  metric_query {
+    id          = "m1"
+    expression  = "SELECT SUM(ProcessedBytes) FROM SCHEMA(\"AWS/NetworkELB\")"
+    return_data = true
+    period      = 300
+  }
+  
+  metric_query {
+    id          = "ad1"
+    expression  = "ANOMALY_DETECTION_BAND(m1, 2)"
+    return_data = true
+  }
+  
+  alarm_actions = [aws_lambda_function.nlb_handler.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "nlb_target_reset_insight_anomaly" {
+  alarm_name          = "nlb-target-reset-insight-anomaly"
+  comparison_operator = "GreaterThanUpperThreshold"
+  evaluation_periods  = 2
+  threshold_metric_id = "ad1"
+  alarm_description   = "Anomaly detection for TCP_Target_Reset_Count across all NLBs"
+  treat_missing_data  = "notBreaching"
+  
+  metric_query {
+    id          = "m1"
+    expression  = "SELECT SUM(TCP_Target_Reset_Count) FROM SCHEMA(\"AWS/NetworkELB\")"
+    return_data = true
+    period      = 300
+  }
+  
+  metric_query {
+    id          = "ad1"
+    expression  = "ANOMALY_DETECTION_BAND(m1, 2)"
+    return_data = true
+  }
+  
+  alarm_actions = [aws_lambda_function.nlb_handler.arn]
+}
